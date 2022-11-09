dd631adba1834a05a860ef1bac0af054d65dd3d0
Ernad Husremović <hernad@bring.out.ba>
2022-11-09 10:54:11 +0100

    updates

--- models/hr_contract.py
+++ models/hr_contract.py
@@ -4,7 +4,7 @@ class HrContract(models.Model):
     _inherit = 'hr.contract'
     _description = "Employee Contract"
 
-    br_bod = fields.Float(string="Broj bodova", digits="Payroll", default=1.0)
+    br_bod = fields.Float(string="Broj bodova po satu", digits="Payroll", default=1.0)
     koef_lo = fields.Float(string="Koef ličnog odbitka", digits="Payroll", default=0.0)
     koef_mr = fields.Float(string="Koef minulog rada", digits="Payroll", default=0.0)
 
